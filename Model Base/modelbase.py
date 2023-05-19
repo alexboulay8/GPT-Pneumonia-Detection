a684e9a2e9569059bc5626a25076c90332009166
Alex Boulay <119967694+alexboulay8@users.noreply.github.com>
2023-05-19 11:29:18 -0400

    change

---
+++ Model Base/modelbase.py
@@ -0,0 +1,208 @@
+from tqdm import tqdm
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from torchvision import transforms
+
+
+import medmnist
+from medmnist import INFO, Evaluator
+from PIL import Image
+
+data_flag = 'pneumoniamnist'
+# data_flag = 'npz file name eg. pneumoniamnist'
+download = True
+
+NUM_EPOCHS = 3
+BATCH_SIZE = 32
+lr = 0.001
+
+info = INFO[data_flag]
+task = info['task']
+n_channels = info['n_channels']
+num_classes = len(info['label'])
+
+
+DataClass = getattr(medmnist, info['python_class'])
+
+# preprocessing
+data_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[.5], std=[.5])
+])
+
+# load the data
+train_dataset = DataClass(split='train', transform=data_transform, download=download)
+test_dataset = DataClass(split='test', transform=data_transform, download=download)
+
+pil_dataset = DataClass(split='train', download=download)
+
+# encapsulate data into dataloader form
+train_loader = data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+train_loader_at_eval = data.DataLoader(dataset=train_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
+test_loader = data.DataLoader(dataset=test_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
+
+train_dataset.montage(length=1)
+train_dataset.montage(length=20)
+
+# define a simple CNN model
+
+class Net(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(Net, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels, 16, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU())
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 16, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(16, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.fc = nn.Sequential(
+            nn.Linear(1024, 128),  # Adjusted input size to 1024
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes))
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+model = Net(in_channels=n_channels, num_classes=num_classes)
+    
+# define loss function and optimizer
+if task == "multi-label, binary-class":
+    criterion = nn.BCEWithLogitsLoss()
+else:
+    criterion = nn.CrossEntropyLoss()
+    
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+# train
+
+for epoch in range(NUM_EPOCHS):
+    train_correct = 0
+    train_total = 0
+    test_correct = 0
+    test_total = 0
+    
+    model.train()
+    for inputs, targets in tqdm(train_loader):
+        # forward + backward + optimize
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        
+        if task == 'multi-label, binary-class':
+            targets = targets.to(torch.float32)
+            loss = criterion(outputs, targets)
+        else:
+            targets = targets.squeeze().long()
+            loss = criterion(outputs, targets)
+        
+        loss.backward()
+        optimizer.step()
+
+# evaluation
+
+def test(split):
+    model.eval()
+    y_true = torch.tensor([])
+    y_score = torch.tensor([])
+    
+    data_loader = train_loader_at_eval if split == 'train' else test_loader
+
+    with torch.no_grad():
+        for inputs, targets in data_loader:
+            outputs = model(inputs)
+
+            if task == 'multi-label, binary-class':
+                targets = targets.to(torch.float32)
+                outputs = outputs.softmax(dim=-1)
+            else:
+                targets = targets.squeeze().long()
+                outputs = outputs.softmax(dim=-1)
+                targets = targets.float().resize_(len(targets), 1)
+
+            y_true = torch.cat((y_true, targets), 0)
+            y_score = torch.cat((y_score, outputs), 0)
+
+        y_true = y_true.numpy()
+        y_score = y_score.detach().numpy()
+        
+        evaluator = Evaluator(data_flag, split)
+        metrics = evaluator.evaluate(y_score)
+    
+        print('%s  auc: %.3f  acc:%.3f' % (split, *metrics))
+
+        
+print('==> Evaluating ...')
+test('train')
+test('test')
+
+
+torch.save(model, r'C:\Users\mgbou\OneDrive\Documents\GitHub\GPT-Pneumonia-Detection\pneumonia_model.pth')
+
+
+model = torch.load(r'C:\Users\mgbou\OneDrive\Documents\GitHub\GPT-Pneumonia-Detection\pneumonia_model.pth')
+
+model.eval()
+
+# Prediction module
+
+def predict_image(model, image_path):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.eval()
+
+    image = Image.open(image_path).convert('L')
+    image_transform = transforms.Compose([
+        transforms.Resize((28, 28)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[.5], std=[.5])
+    ])
+    image = image_transform(image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        outputs = model(image)
+    _, predicted = outputs.max(1)
+    return predicted.item()
+
+
+
+# Usage example
+test_image_path = (r"C:\Users\mgbou\OneDrive\Documents\GitHub\GPT-Pneumonia-Detection\test_images\IM-0011-0001.jpeg")
+prediction = predict_image(model, test_image_path)
+print(f'Prediction: {"Pneumonia" if prediction == 1 else "Non-Pneumonia"}')
+
+
