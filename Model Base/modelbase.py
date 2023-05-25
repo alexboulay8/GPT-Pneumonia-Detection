c5a6468689ab418780b676d0d56848fb422048c9
Alex Boulay <119967694+alexboulay8@users.noreply.github.com>
2023-05-25 09:42:34 -0400

    change to model

--- Model Base/modelbase.py
+++ Model Base/modelbase.py
@@ -1,217 +1,153 @@
-from tqdm import tqdm
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
+from torch import nn, optim
+from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
-from torchvision import transforms
 import medmnist
 from medmnist import INFO, Evaluator
 from PIL import Image
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
+dataset_name = 'pneumoniamnist'
+download_data = True
+learning_rate = 0.001
+num_epochs = 3
+batch_size = 32
 
+dataset_info = INFO[dataset_name]
+task_type = dataset_info['task']
+num_channels = dataset_info['n_channels']
+num_classes = len(dataset_info['label'])
 
+DatasetClass = getattr(medmnist, dataset_info['python_class'])
 
-
-data_flag = 'pneumoniamnist'
-# data_flag = 'npz file name eg. pneumoniamnist'
-download = True
-
-NUM_EPOCHS = 3 #adjust how many cycles through training set model completes
-BATCH_SIZE = 32 #adjust how many training images in each batch model trains with at a time
-lr = 0.001
-
-info = INFO[data_flag]
-task = info['task']
-n_channels = info['n_channels']
-num_classes = len(info['label'])
-
-
-DataClass = getattr(medmnist, info['python_class'])
-
-# preprocessing
-data_transform = transforms.Compose([
+data_preprocessing = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[.5], std=[.5])
 ])
 
-# load the data
-train_dataset = DataClass(split='train', transform=data_transform, download=download)
-test_dataset = DataClass(split='test', transform=data_transform, download=download)
+training_dataset = DatasetClass(split='train', transform=data_preprocessing, download=download_data)
+testing_dataset = DatasetClass(split='test', transform=data_preprocessing, download=download_data)
 
-pil_dataset = DataClass(split='train', download=download)
+training_data_loader = DataLoader(dataset=training_dataset, batch_size=batch_size, shuffle=True)
+eval_data_loader = DataLoader(dataset=training_dataset, batch_size=2*batch_size, shuffle=False)
+testing_data_loader = DataLoader(dataset=testing_dataset, batch_size=2*batch_size, shuffle=False)
 
-# encapsulate data into dataloader form
-train_loader = data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-train_loader_at_eval = data.DataLoader(dataset=train_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
-test_loader = data.DataLoader(dataset=test_dataset, batch_size=2*BATCH_SIZE, shuffle=False)
+training_dataset.montage(length=1)
+training_dataset.montage(length=20)
 
-train_dataset.montage(length=1)
-train_dataset.montage(length=20)
-
-# define a simple CNN model
-
-class Net(nn.Module):
+class Network(nn.Module):
     def __init__(self, in_channels, num_classes):
-        super(Net, self).__init__()
+        super(Network, self).__init__()
 
-        self.layer1 = nn.Sequential(
+        self.conv_layer = nn.Sequential(
             nn.Conv2d(in_channels, 16, kernel_size=3),
             nn.BatchNorm2d(16),
-            nn.ReLU())
-
-        self.layer2 = nn.Sequential(
+            nn.ReLU(),
             nn.Conv2d(16, 16, kernel_size=3),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-
-        self.layer3 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(16, 64, kernel_size=3),
             nn.BatchNorm2d(64),
-            nn.ReLU())
-
-        self.layer4 = nn.Sequential(
+            nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3),
             nn.BatchNorm2d(64),
-            nn.ReLU())
-
-        self.layer5 = nn.Sequential(
+            nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
 
-        self.fc = nn.Sequential(
-            nn.Linear(1024, 128),  # Adjusted input size to 1024
+        self.fc_layer = nn.Sequential(
+            nn.Linear(1024, 128),  
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
-            nn.Linear(128, num_classes))
+            nn.Linear(128, num_classes)
+        )
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
+        x = self.conv_layer(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.fc_layer(x)
         return x
 
+network = Network(in_channels=num_channels, num_classes=num_classes)
 
-model = Net(in_channels=n_channels, num_classes=num_classes)
+criterion = nn.BCEWithLogitsLoss() if task_type == "multi-label, binary-class" else nn.CrossEntropyLoss()
+optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=0.9)
+training_loss = []
+training_accuracy = []
+
+for epoch in range(num_epochs):
+    total_loss = 0.0
+    total_correct = 0
+    counter = 0
+    network.train()
     
-# define loss function and optimizer
-if task == "multi-label, binary-class":
-    criterion = nn.BCEWithLogitsLoss()
-else:
-    criterion = nn.CrossEntropyLoss()
-    
-optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
-
-#declare arrays to hold loss and accuracy values
-train_loss,train_acc = [],[]
-
-
-# train
-for epoch in range(NUM_EPOCHS):
-    train_correct = 0
-    train_total = 0
-    test_correct = 0
-    test_total = 0
-    
-    running_loss = 0.0
-    running_correct,counter = 0,0
-
-    
-    model.train()
-    for inputs, targets in tqdm(train_loader):
-        # forward + backward + optimize
+    for inputs, targets in tqdm(training_data_loader):
         optimizer.zero_grad()
-        outputs = model(inputs)
-        
-        if task == 'multi-label, binary-class':
-            targets = targets.to(torch.float32)
-            loss = criterion(outputs, targets)
-        else:
-            targets = targets.squeeze().long()
-            loss = criterion(outputs, targets)
-        
-        #running loss for current epoch
-        running_loss += loss.item()
-        
-        #running correct for current epoch
-        _,preds = torch.max(outputs.data,1)
-        running_correct += (preds==targets).sum().item()
-      
-        
+        predictions = network(inputs)
+        targets = targets.to(torch.float32) if task_type == 'multi-label, binary-class' else targets.squeeze().long()
+        loss = criterion(predictions, targets)
+        total_loss += loss.item()
+        _, predicted = torch.max(predictions.data, 1)
+        total_correct += (predicted == targets).sum().item()
         loss.backward()
         optimizer.step()
-        counter +=1
-       
-    #calculate epoch loss and accuracy and save to arrays
-    epoch_loss = running_loss / counter
-    epoch_acc = 100.*(running_correct/len(train_loader.dataset))
-    train_loss.append(epoch_loss)
-    train_acc.append(epoch_acc)
-                     
-# evaluation
+        counter += 1
+        
+    epoch_loss = total_loss / counter
+    epoch_accuracy = 100. * (total_correct / len(training_data_loader.dataset))
+    training_loss.append(epoch_loss)
+    training_accuracy.append(epoch_accuracy)
 
-def test(split):
-    model.eval()
-    y_true = torch.tensor([])
-    y_score = torch.tensor([])
-    
-    data_loader = train_loader_at_eval if split == 'train' else test_loader
+def evaluate(split):
+    network.eval()
+    actual_labels = torch.tensor([])
+    predicted_scores = torch.tensor([])
+    data_loader = eval_data_loader if split == 'train' else testing_data_loader
 
     with torch.no_grad():
         for inputs, targets in data_loader:
-            outputs = model(inputs)
-
-            if task == 'multi-label, binary-class':
+            predictions = network(inputs)
+            if task_type == 'multi-label, binary-class':
                 targets = targets.to(torch.float32)
-                outputs = outputs.softmax(dim=-1)
+                predictions = predictions.softmax(dim=-1)
             else:
                 targets = targets.squeeze().long()
-                outputs = outputs.softmax(dim=-1)
+                predictions = predictions.softmax(dim=-1)
                 targets = targets.float().resize_(len(targets), 1)
-
-            y_true = torch.cat((y_true, targets), 0)
-            y_score = torch.cat((y_score, outputs), 0)
-
-        y_true = y_true.numpy()
-        y_score = y_score.detach().numpy()
+            actual_labels = torch.cat((actual_labels, targets), 0)
+            predicted_scores = torch.cat((predicted_scores, predictions), 0)
+        actual_labels = actual_labels.numpy()
+        predicted_scores = predicted_scores.detach().numpy()
         
-        evaluator = Evaluator(data_flag, split)
-        metrics = evaluator.evaluate(y_score)
-    
-        print('%s  auc: %.3f  acc:%.3f' % (split, *metrics))
+        evaluator = Evaluator(dataset_name, split)
+        metrics = evaluator.evaluate(predicted_scores)
+        print(f'{split}  auc: {metrics[0]:.3f}  acc: {metrics[1]:.3f}')
 
-        
 print('==> Evaluating ...')
-test('train')
-test('test')
+evaluate('train')
+evaluate('test')
 
-torch.save(model, r"Path to Directory to save Model (.pth file)")
+torch.save(network, r"Path To Directory (.pth file)")
 
-# Create graph and display graph of training loss and accuracy
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
-plt.plot(range(1, NUM_EPOCHS + 1), train_loss, label='Training Loss')
+plt.plot(range(1, num_epochs + 1), training_loss, label='Training Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend()
 
 plt.subplot(1, 2, 2)
-plt.plot(range(1, NUM_EPOCHS + 1), train_acc, label='Training Accuracy')
+plt.plot(range(1, num_epochs + 1), training_accuracy, label='Training Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.legend()
 
 plt.tight_layout()
 plt.show()
-
-
